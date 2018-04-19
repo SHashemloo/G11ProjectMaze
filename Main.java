0549a675ee58531645390c7bcd58048779e31101
Leyla Samiee <lsamie@gmail.com>
2018-04-19 11:04:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ package com.company;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         try {
-            new MyMaze("map1.txt"); // args [0] if you want filename from command line
+            System.out.println("File to be read is " + args[0] + "\n");
+            new MyMaze(args[0]); // args [0] if you want filename from command line
             System.out.println("File has been output!");
         } catch (Exception e) {
             System.out.println("ERROR : "+e.getMessage());
