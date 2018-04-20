0e018e341f626b86da6c6701e57936076a056b39
Leyla Samiee <lsamie@gmail.com>
2018-04-19 20:42:42 -0400

    Update MyMaze.java

--- MyMaze.java
+++ MyMaze.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class MyMaze {
 
     private char[][] mapText;
+    char workingnMap[][];
     String [] newMazFile = new String[100];
     private int[][] trapsAndLocations = new int[2][100];
     private int round, tStep = 0;
@@ -33,19 +34,14 @@ public class MyMaze {
             this.rows = this.cols = mapSide;
             mapText = new char[mapSide][mapSide];
             mapText[0] = currentLine.toCharArray();
-            System.out.println("currentline at 0 is " +  currentLine);
             int linecounter = 1;
             while (myFileScanner.hasNextLine()) {
                 currentLine = myFileScanner.nextLine();
-                System.out.println("currentline at " + linecounter + " is " +  currentLine);
                 mapText[linecounter] = currentLine.toCharArray();
-                /* for (int i = 0; i < mapSide; i++) {
-                    System.out.println("mapText at " + linecounter + " and " + i + " is " +  mapText[linecounter][i]);
-                } */
                 linecounter++;
             }
             System.out.print("Done filling the Array \n total of " + this.trapCount + " traps ");
-            // make sure our array is correct;
+            // Write the map into working map and count the traps
             for (int i = 0; i < mapSide; i++) {
                 for (int j = 0; j < mapSide; j++) {
                     if (mapText[i][j] == 'P') {
@@ -122,12 +118,57 @@ public class MyMaze {
     //assuming all maps are square
     //' ' is where the monster can move
     // '+', '-'. '|' are obstacles
+    // we skip outer walls
     //'N' is start position
     //'F' is food
     //'P' is potential trap location
     //x is column, y is row
     //mark the path in the maze via symbol 'S'
     //if x = < 0, or y < 0 or x or y > mapSide (out of bounds) return false
+
+    boolean solveChildMaze(String mazeName, char [][] mapText, int row, int col) {
+
+        char right = mapText[row][col + 1];
+        char left = mapText[row][col - 1];
+        char up = mapText[row - 1][col];
+        char down = mapText[row + 1][col];
+        int cstepsTaken = 1;
+        String outMaze = new String (mazeName + ".out");
+        // System.out.print("Solving for " + mazeName + " starting from " + row + " and " + col + " writing in " +
+        //                outMaze +  "\n");
+        // System.out.print("Solving for " + row + " and " + col + " \n");
+        if (right == 'F' || left == 'F' || up == 'F' || down == 'F') { // we are beside the Exit
+            mapText[row][col] = 'S';
+            cstepsTaken++;
+            System.out.println("Step #" + cstepsTaken);
+            return true; // return true once we reach the destination.
+        } else {
+            System.out.print("Stepping in " + row + " and " + col + " \n");
+            mapText[row][col] = 'S';
+            cstepsTaken++;
+        }
+
+        boolean solved = false;
+
+        if ((right == 'P' || right == ' ') && !solved) {
+            solved = solveChildMaze(mazeName, mapText, row, col + 1);
+        }
+        if ((down == ' ' || down == 'P') && !solved) {
+            solved = solveChildMaze(mazeName, mapText,row + 1, col);
+        }
+        if ((left == ' ' || left == 'P') && !solved) {
+            solved = solveChildMaze(mazeName, mapText, row, col - 1);
+        }
+        if ((up == ' ' || up == 'P') && !solved) {
+            solved = solveChildMaze(mazeName, mapText,row - 1, col);
+        }
+        if (!solved) {
+            mapText[row][col] = ' '; // unsolved path are no good. so lets clear our mark.
+        }
+        return solved;
+
+    }
+
 
     boolean solve(String mazeName, int row, int col) {
 
@@ -136,8 +177,8 @@ public class MyMaze {
         char up = this.mapText[row - 1][col];
         char down = this.mapText[row + 1][col];
         String outMaze = new String (mazeName + ".out");
-        System.out.print("Solving for " + mazeName + " starting from " + row + " and " + col + " writing in " +
-                        outMaze +  "\n");
+        // System.out.print("Solving for " + mazeName + " starting from " + row + " and " + col + " writing in " +
+        //                outMaze +  "\n");
         // System.out.print("Solving for " + row + " and " + col + " \n");
         if (right == 'F' || left == 'F' || up == 'F' || down == 'F') { // we are beside the Exit
             this.mapText[row][col] = 'S';
@@ -167,9 +208,9 @@ public class MyMaze {
         }
 
         boolean solved = false;
-/* 
+/*
  * if (this.mapText[row][col] != 'P') {
-   this.mapText[row][col] = 'S'; // we want to keep the potential traps value of 'P' 
+   this.mapText[row][col] = 'S'; // we want to keep the potential traps value of 'P'
   }
   */
 
@@ -189,6 +230,7 @@ public class MyMaze {
             this.mapText[row][col] = ' '; // unsolved path are no good. so lets clear our mark.
         }
         return solved;   // return false if nothing matches.(execute in one branch)
-        // I suppose all solved: boolean will return false and the solve() function will finally return false when 
+        // I suppose all solved: boolean will return false and the solve() function will finally return false when
     }       // no path exists. Eventually every path will lead to dead end.
+
 }
