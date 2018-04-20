877844688df2b3d4621b5ed9c48d9a474958c312
Leyla Samiee <lsamie@gmail.com>
2018-04-19 23:35:45 -0400

    Update MyMaze.java

--- MyMaze.java
+++ MyMaze.java
@@ -10,6 +10,8 @@ public class MyMaze {
 
     private char[][] mapText;
     char workingnMap[][];
+    char solution[][];
+
     String [] newMazFile = new String[100];
     private int[][] trapsAndLocations = new int[2][100];
     private int round, tStep = 0;
@@ -18,8 +20,9 @@ public class MyMaze {
     int stepsTaken = 1;
     int rowStart = 1, colStart = 1;
     private int rows, cols;
-    private String outputFilename;
-    private String inputFilename;
+    int mapSize = 0;
+    String outputFilename;
+    String inputFilename;
 
     public MyMaze (String filename) throws IOException {
         try {
@@ -33,21 +36,28 @@ public class MyMaze {
             mapSide = currentLine.length();
             this.rows = this.cols = mapSide;
             mapText = new char[mapSide][mapSide];
+            solution = new char[mapSide][mapSide];
             mapText[0] = currentLine.toCharArray();
+            solution[0] = currentLine.toCharArray();
             int linecounter = 1;
+
+            // fill the internal 2d array with the original file
+
             while (myFileScanner.hasNextLine()) {
                 currentLine = myFileScanner.nextLine();
                 mapText[linecounter] = currentLine.toCharArray();
+                solution[linecounter] = currentLine.toCharArray();
+
                 linecounter++;
             }
             System.out.print("Done filling the Array \n total of " + this.trapCount + " traps ");
-            // Write the map into working map and count the traps
+            // Write the map into main map and count the traps
             for (int i = 0; i < mapSide; i++) {
                 for (int j = 0; j < mapSide; j++) {
                     if (mapText[i][j] == 'P') {
                         // Hold the position of this round's potential Trap
                         // we will add the step counted of this potential Trap in tStep
-                        this.trapsAndLocations[0][this.trapCount] = i*10+j;
+                        this.trapsAndLocations[0][this.trapCount] = i*10000+j;
                         System.out.println("found a trap " + this.trapsAndLocations[0][this.trapCount]);
                         this.trapCount++;
                     }
@@ -56,23 +66,11 @@ public class MyMaze {
                 System.out.print("\n");
             }
 
+            this.mapSize = mapSide;
+
             System.out.println("found total of " + this.trapCount + " traps ");
             System.out.println("trapCounter is " + this.trapCounter + " before the loop ");
-            for (int i = 0; i++ < this.trapCount;) {
-                System.out.println("writing the input file #" + i + "!! ");
-                System.out.println("trapCounter is " + this.trapCounter + "!! ");
-                this.newMazFile[i] = createInputFile(i);
-                // this.trapCounter++;
-            }
-            System.out.println("total trapCount is " + this.trapCount + ":) ");
-            for (int i = 0; i++ < this.trapCount;) {
-                System.out.println("file name #" + i + " is " + this.newMazFile[i]);
-            }
-            /* for (int i = 0; i++ < this.trapCount;) {
-                this.stepsTaken = 1;
-                solve(this.newMazFile[i], this.rowStart, this.colStart);
-            } */
-
+        
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("ERROR : " + e.getMessage());
@@ -126,49 +124,36 @@ public class MyMaze {
     //mark the path in the maze via symbol 'S'
     //if x = < 0, or y < 0 or x or y > mapSide (out of bounds) return false
 
-    boolean solveChildMaze(String mazeName, char [][] mapText, int row, int col) {
+    char [][] fillWorkingMap (int mapSide, String filename) {
+        try {
+            File map = new java.io.File(filename);
+            Scanner myFileScanner = new Scanner(map);
+            String currentLine;
+            currentLine = myFileScanner.nextLine();
+            workingnMap = new char[mapSide][mapSide];
+            workingnMap[0] = currentLine.toCharArray();
+            int lineno = 1;
+        
+            // Write the map into main map and count the traps
+            while (myFileScanner.hasNextLine()) {
+                currentLine = myFileScanner.nextLine();
+                workingnMap[lineno] = currentLine.toCharArray();
+                lineno++;
+            }
+            System.out.print("dumping the working Map\n");
+            for (int i = 0; i < mapSide; i++) {
+                for (int j = 0; j < mapSide; j++) {
+                    System.out.print(mapText[i][j]);
+                }
+                System.out.print("\n");
+            }
 
-        char right = mapText[row][col + 1];
-        char left = mapText[row][col - 1];
-        char up = mapText[row - 1][col];
-        char down = mapText[row + 1][col];
-        int cstepsTaken = 1;
-        String outMaze = new String (mazeName + ".out");
-        // System.out.print("Solving for " + mazeName + " starting from " + row + " and " + col + " writing in " +
-        //                outMaze +  "\n");
-        // System.out.print("Solving for " + row + " and " + col + " \n");
-        if (right == 'F' || left == 'F' || up == 'F' || down == 'F') { // we are beside the Exit
-            mapText[row][col] = 'S';
-            cstepsTaken++;
-            System.out.println("Step #" + cstepsTaken);
-            return true; // return true once we reach the destination.
-        } else {
-            System.out.print("Stepping in " + row + " and " + col + " \n");
-            mapText[row][col] = 'S';
-            cstepsTaken++;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("ERROR : " + e.getMessage());
         }
-
-        boolean solved = false;
-
-        if ((right == 'P' || right == ' ') && !solved) {
-            solved = solveChildMaze(mazeName, mapText, row, col + 1);
-        }
-        if ((down == ' ' || down == 'P') && !solved) {
-            solved = solveChildMaze(mazeName, mapText,row + 1, col);
-        }
-        if ((left == ' ' || left == 'P') && !solved) {
-            solved = solveChildMaze(mazeName, mapText, row, col - 1);
-        }
-        if ((up == ' ' || up == 'P') && !solved) {
-            solved = solveChildMaze(mazeName, mapText,row - 1, col);
-        }
-        if (!solved) {
-            mapText[row][col] = ' '; // unsolved path are no good. so lets clear our mark.
-        }
-        return solved;
-
+        return workingnMap;
     }
-
 
     boolean solve(String mazeName, int row, int col) {
 
@@ -176,12 +161,18 @@ public class MyMaze {
         char left = this.mapText[row][col - 1];
         char up = this.mapText[row - 1][col];
         char down = this.mapText[row + 1][col];
-        String outMaze = new String (mazeName + ".out");
+        String outMaze = new String();
+        String solutionFile = new String();
+        solutionFile =  "solution.txt";
+        // File fileSol;
+        // PrintWriter writerSol;
+        outMaze = mazeName + ".out";
         // System.out.print("Solving for " + mazeName + " starting from " + row + " and " + col + " writing in " +
-        //                outMaze +  "\n");
+        //             outMaze +  "\n");
         // System.out.print("Solving for " + row + " and " + col + " \n");
         if (right == 'F' || left == 'F' || up == 'F' || down == 'F') { // we are beside the Exit
             this.mapText[row][col] = 'S';
+            this.solution[row][col] = ' ';
             this.stepsTaken++;
             System.out.println("Step #" + this.stepsTaken);
             try {
@@ -191,28 +182,42 @@ public class MyMaze {
                 PrintWriter writer = new PrintWriter(file);
                 for (int i = 0; i < this.rows; i++) {
                     for (int j = 0; j < this.cols; j++) {
-                        // System.out.print("writing the char value of " + this.mapText[i][j] + "\n");
                         writer.print(this.mapText[i][j]);
                     }
                     writer.println();
                 }
                 writer.close();
+
+                File fileSol = new File(solutionFile);  // creating the output filename
+                System.out.print("writing the file " + solutionFile + " for the maze " + mazeName + " by taking "
+                        + this.stepsTaken + " \n");
+                PrintWriter writerSol = new PrintWriter(fileSol);
+                for (int i = 0; i < this.rows; i++) {
+                    for (int j = 0; j < this.cols; j++) {
+                        System.out.print(this.solution[i][j]);
+                        writerSol.print(this.solution[i][j]);
+                    }
+                    System.out.println();
+                    writerSol.println();
+                }
+                writerSol.close();
             } catch (FileNotFoundException e) {
                 System.out.println("ERROR : " + e.getMessage());
             }
             return true; // return true once we reach the destination.
         } else {
-            System.out.print("Stepping in " + row + " and " + col + " \n");
-            this.mapText[row][col] = 'S';
+            // System.out.print("Stepping in " + row + " and " + col + " \n");
+            if (this.mapText[row][col] == 'P') {
+                this.mapText[row][col] = 'T';
+                this.solution[row][col] = 'T';
+            } else {
+                this.mapText[row][col] = 'S';
+                this.solution[row][col] = ' ';
+            }
             this.stepsTaken++;
         }
 
         boolean solved = false;
-/*
- * if (this.mapText[row][col] != 'P') {
-   this.mapText[row][col] = 'S'; // we want to keep the potential traps value of 'P'
-  }
-  */
 
         if ((right == 'P' || right == ' ') && !solved) {
             solved = solve(mazeName, row, col + 1);
@@ -228,9 +233,8 @@ public class MyMaze {
         }
         if (!solved) {
             this.mapText[row][col] = ' '; // unsolved path are no good. so lets clear our mark.
+            this.solution[row][col] = ' ';
         }
-        return solved;   // return false if nothing matches.(execute in one branch)
-        // I suppose all solved: boolean will return false and the solve() function will finally return false when
-    }       // no path exists. Eventually every path will lead to dead end.
-
+        return solved;
+    }
 }
