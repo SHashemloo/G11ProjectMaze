da7d45f5fd56718daebb607ffaf5e30d93f7522f
Leyla Samiee <lsamie@gmail.com>
2018-04-19 20:44:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             System.out.println("File to be read is " + args[0] + "\n");
-            new MyMaze(args[0]); // args [0] if you want filename from command line
+            MyMaze mazeRunner = new MyMaze(args[0]); // args [0] if you want filename from command line
+            for (int i = 0; i++ < mazeRunner.trapCount;) {
+                System.out.print("Solving for " + mazeRunner.newMazFile[i] + " starting from " + mazeRunner.rowStart +
+                        " writing in " + mazeRunner.colStart +  "\n");
+                mazeRunner.stepsTaken = 1;
+                mazeRunner.solve(mazeRunner.newMazFile[i], mazeRunner.rowStart, mazeRunner.colStart);
+                System.out.println("solved the maze for file: " + mazeRunner.newMazFile[i]);
+            }
             System.out.println("File has been output!");
         } catch (Exception e) {
             System.out.println("ERROR : "+e.getMessage());
